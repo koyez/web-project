12a0e71dce16691fe5c9decaa5c56558edd7a1a8
koyez <actmartly@live.com>
2021-12-05 21:09:44 +0800

    feat(typescript): add original data type

---
+++ JavaScript/learn-typescript/base/arbitrary-value/arbitrary.ts
@@ -0,0 +1,6 @@
+// 任意值（Any）表示可以赋值为任意类型值
+let notAnyValue: string = "senven";
+// notAnyValue = 7;    // error
+
+let anyValue: any = "senven";
+anyValue = 7;   // ok
---
+++ JavaScript/learn-typescript/base/original-data-type/null-undefined.ts
@@ -0,0 +1,4 @@
+let u: undefined = undefined;
+
+// null 和 undefined 是所有数据类型的原型，null和undefined可以赋值给其他类型
+let v: number = u;
